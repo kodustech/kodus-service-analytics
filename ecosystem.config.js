ee46e6e5d2a8c05c20acff7bbf494ea7595f77c2
wellington01 <wellington.c.v.santana@gmail.com>
2025-02-16 13:32:55 -0300

    more fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'kodus-service-analytics',
-      script: 'dist/src/main.js',
+      script: 'dist/index.js',
       instances: 1,
       autorestart: true,
       watch: false,
